4314cd9e6fd47ea3740038c5e1cde01b9d4eeef5
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-05-06 16:57:46 -0600

    removes routes folder

--- routes/products.js
+++
@@ -1,9 +0,0 @@
-const Router = require('express-promise-router');
-const db = require('../db');
-const router = new Router();
-module.exports = router;
-
-router.get('/products', async (req, res) => {
-  const { rows } = await db.query('SELECT * FROM products');
-  res.send(rows);
-});
