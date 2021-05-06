5659bf84abd55f9916a3ac8ea0fc30b6d7270e99
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-05-06 16:16:48 -0600

    adds route for products

--- routes/products.js
+++ routes/products.js
@@ -6,8 +6,9 @@ const db = require('../db')
 const router = new Router()
 // export our router to be mounted by the parent application
 module.exports = router
-router.get('/:id', async (req, res) => {
+
+router.get('/products', async (req, res) => {
   const { id } = req.params
-  const { rows } = await db.query('SELECT * FROM users WHERE id = $1', [id])
-  res.send(rows[0])
-})
+  const { rows } = await db.query('SELECT * FROM products')
+  res.send(rows)
+});
--- server/index.js
+++ server/index.js
@@ -1,11 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const db = require('../db');
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Static files served');
+});
+
+app.get('/products', (rew, res) => {
+  db.query('SELECT * FROM products')
+  .then(results => {
+    res.send(JSON.stringify(results));
+  })
+  .catch(err => {
+    console.log('ERROR: ', err);
+    res(JSON.stringify(err));
+  });
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
+
+
