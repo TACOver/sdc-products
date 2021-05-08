e3278e9e17dd6a9ccc68a5b3af690c7d78b9e8a2
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-05-08 11:02:45 -0600

    adds distinct to aggregate functions

--- server/index.js
+++ server/index.js
@@ -61,9 +61,11 @@ app.get('/products/:productId/styles', (req, res) => {
   const SQL = 
     `
       SELECT styles.style_id, style_name, original_price, sale_price, isdefault,
-      jsonb_agg (jsonb_build_object('thumbnail_url',photos.thumbnail,'url',photos.url)) photos
+      jsonb_agg (distinct jsonb_build_object('thumbnail_url',photos.thumbnail,'url',photos.url)) photos,
+      jsonb_agg (distinct jsonb_build_object('sku_id',skus.sku_id,'size',skus.item_size,'quantity', skus.quantity)) skus
       FROM styles
       LEFT JOIN photos ON styles.style_id = photos.style_id
+      INNER JOIN skus ON styles.style_id = skus.style_id
       WHERE product_id=${req.params.productId}
       GROUP BY styles.style_id
     `;
