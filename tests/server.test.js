c50c144838c54dc17944c80c9c091eda6ebfce19
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-05-10 10:46:16 -0600

    add supertest to dev dependecies

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,4 +1,10 @@
-const app = require('../server/routes');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const routes = require('./routes')(app);
+
+app.use(cors());
+const port = 2999;
 
 describe('Products route', () => {
 
