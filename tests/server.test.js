2709b9635be2cca386a5a53175ee32227e9361b0
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-05-10 11:05:06 -0600

    has working tests using supertest

--- tests/server.test.js
+++ tests/server.test.js
@@ -1,11 +1,33 @@
-const express = require('express');
+const request = require('supertest');
 const cors = require('cors');
-const app = express();
-const routes = require('./routes')(app);
+const express = require('express');
 
+const app = express();
+const routes = require('../server/routes')(app);
 app.use(cors());
-const port = 2999;
 
 describe('Products route', () => {
 
+  it('should get 5 products on a products fetch with no parameters', () => {
+    request(app)
+      .get('/products')
+      .expect(200)
+      .expect(res => {
+        res.body.length = 5;
+      })
+      .end(function(err, res) {
+        if (err) throw err;
+      });
+  });
+  it('should get the correct number of products on fetch with count parameter', () => {
+    request(app)
+      .get('/products?count=4')
+      .expect(200)
+      .expect(res => {
+        res.body.length = 4;
+      })
+      .end(function(err, res) {
+        if (err) throw err;
+      });
+  });
 });
