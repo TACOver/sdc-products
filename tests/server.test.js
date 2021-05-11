55abef005972e93eb4642832314588290c1d9754
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-05-10 18:12:52 -0600

    installs loadtest in dev depend and globally as root

--- tests/server.test.js
+++ tests/server.test.js
@@ -3,25 +3,43 @@ const app = require('../server/app');
 const db = require('../db');
 
 describe('Products route', () => {
-  afterAll(() => {
-    db.pool.end();
-  });
-
-  it('should get 5 products on a products fetch with no parameters', async () => {
-    await request(app)
+  it('should get 5 products on a products fetch with no parameters', () => {
+    request(app)
       .get('/products')
       .expect(200)
       .expect(res => {
-        res.body.length = 5;
+        res.body.length = 6;
       });
   });
 
-  it('should get the correct number of products on fetch with count parameter', async () => {
-    await request(app)
+  it('should get the correct number of products on fetch with count parameter', () => {
+    request(app)
       .get('/products?count=4')
       .expect(200)
       .expect(res => {
         res.body.length = 4;
       });
   });
+
+  it('should get one product on /products/:productId fetch', () => {
+    request(app)
+      .get('/products/2')
+      .expect(200)
+      .expect(res => {
+        res.body.length = 1;
+      });
+  });
+
+  it('should get 4 related items for a product', (done) => {
+    request(app)
+      .get('/products/2/related')
+      .expect(200)
+      .expect(res => {
+        res.body.length = 6;
+      }, done);
+  });
 });
+
+
+
+// db.pool.end();
